63c88e62a2afaccf50a3a3045ab312a4d411c53d
dajimenez <da.jimenez007@ebc.edu.mx>
2026-08-21 19:26:52 -0600

    actualizacion hasta pedidos/dao/boardgamedao.py

---
+++ pedidos/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+# Create your models here.
+#django ORM - Crear
+
+class Producto(models.Model):
+    CATEGORIAS = [
+        ('FAMILIARES', 'Familiares'),
+        ('PARTY GAMES', 'Party Games'),
+        ('ESTRATEGIA', 'Estrategia'),
+    ]
+    nombre = models.CharField(max_length=100)
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+    categoria = models.CharField(max_length=10, choices=CATEGORIAS)
+    disponible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.nombre} - ${self.precio}"
+
+class Pedido(models.Model):
+    ESTADOS = [
+        ('PREPARANDO', 'Preparando'),
+        ('EN_TRANSITO', 'En Tránsito'),
+        ('LISTO', 'Listo para Entrega'),
+        ('ENTREGADO', 'Entregado'),
+    ]
+    cliente_nombre = models.CharField(max_length=100)
+    fecha = models.DateTimeField(auto_now_add=True)
+    estado = models.CharField(max_length=15, choices=ESTADOS, default='PREPARANDO')
+    total = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return f"Orden #{self.id} - {self.cliente_nombre} ({self.estado})"
